76cbb3344a8ef36903c42b83d82f603f8837075b
juteman <juteman_open@yahoo.com>
2018-02-02 20:52:34 +0800

    init-packages

---
+++ lisp/init-packages.el
@@ -0,0 +1,10 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                           ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+
+
+
+
+(provide 'init-packages)
