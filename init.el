06af9e437bf6493e63c0bf4904ae2503fabd3b80
juteman <juteman_open@yahoo.com>
2018-02-02 20:34:19 +0800

    ui和package

---
+++ init.el
@@ -0,0 +1,10 @@
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; 插件包管理
+;; ------------分割线-----------------------------------------------------
+
+(require 'init-packages)
+(require 'init-ui)
+
