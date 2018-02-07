90ae74b1437ef83dbcfbe83c6977b5e7a3d7e0b6
juteman <juteman_open@yahoo.com>
2018-02-07 20:15:32 +0800

    snippets

--- elpa/yasnippet-snippets-20180122.521/yasnippet-snippets-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; yasnippet-snippets-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "yasnippet-snippets" "yasnippet-snippets.el"
-;;;;;;  (23162 41892 291473 909000))
-;;; Generated autoloads from yasnippet-snippets.el
-
-(autoload 'yasnippet-snippets-initialize "yasnippet-snippets" "\
-
-
-\(fn)" nil nil)
-
-(eval-after-load 'yasnippet '(yasnippet-snippets-initialize))
-
-;;;***
-
-;;;### (autoloads nil nil ("yasnippet-snippets-pkg.el") (23162 41892
-;;;;;;  298456 361000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yasnippet-snippets-autoloads.el ends here
