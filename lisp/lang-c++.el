27cf37987e75632f3abfd091447032f5c5f9d072
juteman <juteman_open@yahoo.com>
2018-03-05 21:59:40 +0800

    change ac

--- lisp/lang-c++.el
+++ lisp/lang-c++.el
@@ -1,14 +1,13 @@
-;;设置括号匹配以及彩虹括号
+`;;设置括号匹配以及彩虹括号
 (require 'auto-complete-clang)
 (add-hook 'c++-mode-hook #'smartparens-mode)
 (add-hook 'c++-mode-hook #'rainbow-delimiters-mode)
 ;;GUI下使用对齐线
-;;(add-hook 'c-mode-common-hook #'highlight-indentation-mode)
-;;(add-hook 'c-mode-common-hook #'highlight-indentation-current-column-mode)
+(add-hook 'c-mode-common-hook #'highlight-indentation-mode)
+(add-hook 'c-mode-common-hook #'highlight-indentation-current-column-mode)
 (add-hook 'c-mode-common-hook #'smartparens-mode)
 (add-hook 'c-mode-common-hook #'rainbow-delimiters-mode)
 ;;END
-
 ;;YASnippet
 (yas-reload-all)
 (add-hook 'c++-mode-hook #'(lambda() ('yas-minor-mode)))
@@ -16,27 +15,29 @@
 
 ;;自动补全设置
 ;;ac-clang-flags 设置
-(setq ac-clang-flags
-      (mapcar (lambda (item)(concat "-I" item))
-	      (split-string
-	       "
-/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/c++/5.4.0
-/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/x86_64-linux-gnu/c++/5.4.0
-/usr/bin/../lib/gcc/x86_64-linux-gnu/5.4.0/../../../../include/c++/5.4.0/backward
-/usr/local/include
-/usr/lib/llvm-3.8/bin/../lib/clang/3.8.0/include
-/usr/include/x86_64-linux-gnu
-/usr/include
+;;(ac-config-default)
+;;;; (setq ac-clang-flags
+;;       (mapcar (lambda (item)(concat "-I" item))
+;; 	      (split-string
+;; 	       "
+;; /usr/bin/../lib/gcc/x86_64-linux-gnu/7.3.0/../../../../include/c++/7.3.0
+;; /usr/bin/../lib/gcc/x86_64-linux-gnu/7.3.0/../../../../include/x86_64-linux-gnu/c++/7.3.0
+;; /usr/bin/../lib/gcc/x86_64-linux-gnu/7.3.0/../../../../include/c++/7.3.0/backward
+;; /usr/include/clang/4.0.1/include
+;; /usr/local/include
+;; /usr/include/x86_64-linux-gnu
+;; /usr/include
 
-")))
+
+;; ")))
 
 
-(defun my-cc-mode-setup()
-  (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+;; (defun my-cc-mode-setup()
+;;   (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
 
-(add-hook 'c-mode-common-hook 'my-cc-mode-setup)
-(add-hook 'c++-mode-hook 'my-cc-mode-setup)
-;;END
+;; (add-hook 'c-mode-common-hook 'my-cc-mode-setup)
+;; (add-hook 'c++-mode-hook 'my-cc-mode-setup)
+;; ;;END
 
 
 ;;clang-format
