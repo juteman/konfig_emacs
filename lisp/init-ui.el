d68bb6a50b97e3c843ac2054659b17fb300e4b8e
juteman <juteman_open@yahoo.com>
2018-03-06 19:44:39 +0800

    ui

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -6,4 +6,7 @@
 (scroll-bar-mode 0)
 ;;去掉
 (setq inhibit-startup-message t)
+(display-time-mode 1) ;; 常显 
+(setq display-time-24hr-format t) ;;格式 
+(setq display-time-day-and-date t) ;;显示时间、星期、日期
 (provide 'init-ui)
