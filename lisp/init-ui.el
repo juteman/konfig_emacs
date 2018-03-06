dd048f3cc0482cd3a807d37bc4634a9772a3b7ac
juteman <juteman_open@yahoo.com>
2018-03-06 19:43:02 +0800

    change ui

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,3 +1,9 @@
 (setq-default cursor-type 'bar)
 (global-hl-line-mode 0)
+;;去掉工具栏
+(tool-bar-mode 0) 
+(menu-bar-mode 0) 
+(scroll-bar-mode 0)
+;;去掉
+(setq inhibit-startup-message t)
 (provide 'init-ui)
